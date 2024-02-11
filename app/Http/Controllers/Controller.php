574da4c16b52b28883eba16893799036e163eb82
Varga Tamas <vargata1@gmail.com>
2024-02-11 18:40:12 +0100

    basic swagger config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *       title="Movie API",
+ * *     version="1.0.0",
+ * *     description="API for managing movies",
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -18,6 +18,50 @@ class MovieController extends Controller
         $this->movieRepository = $movieRepository;
     }
 
+    /**
+     * @OA\Get (
+     *     path="/api/movies",
+     *     tags={"Movies"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 property="rows",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="_id",
+     *                         type="number",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="title",
+     *                         type="string",
+     *                     ),
+     *                     @OA\Property(
+     *                         property="director",
+     *                         type="string",
+     *                     ),
+     *                     @OA\Property(
+     *                         property="cast",
+     *                         type="string",
+     *                     ),
+     *                     @OA\Property(
+     *                         property="category",
+     *                         type="string",
+     *                     ),
+     *                     @OA\Property(
+     *                         property="releaseDate",
+     *                         type="string",
+     *                      ),
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function index(): AnonymousResourceCollection
     {
         $movies = $this->movieRepository->findAll();
@@ -25,6 +69,30 @@ class MovieController extends Controller
         return MovieResource::collection($movies);
     }
 
+    /**
+     * @OA\Get (
+     *     path="/api/movies/{id}",
+     *     tags={"Movies"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="id", type="number"),
+     *              @OA\Property(property="title", type="string"),
+     *              @OA\Property(property="director", type="string"),
+     *              @OA\Property(property="cast", type="string"),
+     *              @OA\Property(property="category", type="string"),
+     *              @OA\Property(property="releaseDate", type="string")
+     *         )
+     *     )
+     * )
+     */
     public function show(int $movieId): MovieResource
     {
         $movie = $this->movieRepository->findOrFail($movieId);
@@ -33,6 +101,68 @@ class MovieController extends Controller
 
     }
 
+    /**
+     * @OA\Post (
+     *     path="/api/movies",
+     *     tags={"Movies"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="title",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="director",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="cast",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="category",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="releaseDate",
+     *                          type="string"
+     *                       ),
+     *                 ),
+     *                 example={
+     *                     "title":"example title",
+     *                     "director":"example director",
+     *                     "cast":"example cast",
+     *                     "category":"example category",
+     *                     "releaseDate":"YYYY-MM-DD"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="title", type="string", example="title"),
+     *              @OA\Property(property="director", type="string", example="director"),
+     *              @OA\Property(property="cast", type="string", example="cast"),
+     *              @OA\Property(property="category", type="string", example="category"),
+     *              @OA\Property(property="releaseDate", type="string", example="releaseDate"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="invalid",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      )
+     * )
+     */
     public function store(MovieRequest $request): MovieResource
     {
         $movie = $this->movieRepository->store((array)$request->validated());
@@ -40,6 +170,67 @@ class MovieController extends Controller
         return new MovieResource($movie);
     }
 
+    /**
+     * @OA\Put (
+     *     path="/api/movies/{id}",
+     *     tags={"Movies"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="title",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="director",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="cast",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="category",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="releaseDate",
+     *                          type="string"
+     *                       )
+     *                 ),
+     *                 example={
+     *                      "title":"example title",
+     *                      "director":"example director",
+     *                      "cast":"example cast",
+     *                      "category":"example category",
+     *                      "releaseDate":"YYYY-MM-DD"
+     *                 }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="number", example=1),
+     *              @OA\Property(property="title", type="string", example="title"),
+     *              @OA\Property(property="director", type="string", example="director"),
+     *              @OA\Property(property="cast", type="string", example="cast"),
+     *              @OA\Property(property="category", type="string", example="category"),
+     *              @OA\Property(property="releaseDate", type="string", example="2021-12-11T09:25:53.000000Z")
+     *          )
+     *      )
+     * )
+     */
     public function update(MovieRequest $request, int $departmentId): JsonResponse
     {
         $this->movieRepository->update((array)$request->validated(), $departmentId);
@@ -47,6 +238,25 @@ class MovieController extends Controller
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * @OA\Delete (
+     *     path="/api/movies/{id}",
+     *     tags={"Movies"},
+     *     @OA\Parameter(
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="msg", type="string", example="delete movie success")
+     *         )
+     *     )
+     * )
+     */
     public function destroy(int $movieId): JsonResponse
     {
         $this->movieRepository->delete($movieId);
